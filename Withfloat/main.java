1d7521750bcd6863b7aca2e76c474ccfa4d8b40d
Dima <SemenihinDmit@yandex.ru>
2020-10-15 14:13:33 +0300

    added main

---
+++ Withfloat/main.java
@@ -0,0 +1,14 @@
+package Withfloat;
+
+public class main {
+    public static void main(String[] args) {
+        Float a = new Float(23456001,3);
+        Float b = new Float(86487010,3);
+        a.sum(b);
+        a.dif(b);
+
+    }
+}
+
+
+
